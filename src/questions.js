34b2bfc932077aabbf4cd341aae0787a50094261
Christopher Adamson <software.pro.08@gmail.com>
2020-09-18 16:37:55 -0700

    initial commit

---
+++ src/questions.js
@@ -0,0 +1,40 @@
+const questions = [
+    {
+        questionText: 'When was Javascript created?',
+        answerOptions: [
+            { answerText: 'November 1994', isCorrect: false },
+            { answerText: 'October 1993', isCorrect: false },
+            { answerText: 'September 1995', isCorrect: true },
+            { answerText: 'October 1994', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who was president during the Vietnam war?',
+        answerOptions: [
+            { answerText: 'Dwight D. Eisenhower', isCorrect: false },
+            { answerText: 'Lyndon B. Johnson', isCorrect: true },
+            { answerText: 'Bill Clinton', isCorrect: false },
+            { answerText: 'Richard Nixon', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who owns the Android OS?',
+        answerOptions: [
+            { answerText: 'Google', isCorrect: true },
+            { answerText: 'Microsoft', isCorrect: false },
+            { answerText: 'Amazon', isCorrect: false },
+            { answerText: 'Apple', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'When did World War II end?',
+        answerOptions: [
+            { answerText: 'September 2, 1940', isCorrect: false },
+            { answerText: 'November 1, 1935', isCorrect: false },
+            { answerText: 'October 30, 1941', isCorrect: false },
+            { answerText: 'September 2, 1945', isCorrect: true },
+        ],
+    },
+];
+
+export default questions;
